21249a6710d70797b6acd5ddd4a73dc910b33969
Metalax <sampannapokharel@gmail.com>
2023-04-16 01:23:49 +0545

    customized/ added home page content

--- frontend/src/Pages/Home/index.jsx
+++ frontend/src/Pages/Home/index.jsx
@@ -1,17 +1,54 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Cookies from "universal-cookie";
+import { useNavigate } from "react-router-dom";
 import "./style.css";
 
+const cookies = new Cookies();
+
 export default function Home() {
+    const navigate = useNavigate();
+    const userName = JSON.parse(localStorage.getItem("activeUser")).fullName;
+
+    useEffect(() => {
+        const nav = document.getElementById("navigation-bar");
+        console.log(nav.style.backgroundColor);
+        if (nav) {
+            nav.style.backgroundColor = "rgba(255, 255, 255, 0.8)";
+        }
+    });
+
     return (
         <div className="home-page">
-            <section className="home-page-section-1">
-                <h1> Section 1</h1>
-            </section>
-            <section className="home-page-section-2">
-                <h1> Section 2</h1>
-            </section>
-            <section className="home-page-section-3">
-                <h1> Section 3</h1>
+            <section className="home-page-container">
+                <div className="home-page-top">
+                    <label className="home-page-title">
+                        <b>WELCOME</b>,
+                    </label>
+                    <label className="home-page-user">{userName}</label>
+                    <label className="home-page-text">
+                        What do you want to do today
+                    </label>
+                </div>
+                <div className="home-page-bottom">
+                    <div
+                        className="home-page-card home-page-card-1"
+                        onClick={() => navigate("/tutorial")}
+                    >
+                        Tutorial
+                    </div>
+                    <div
+                        className="home-page-card home-page-card-2"
+                        onClick={() => navigate("/test")}
+                    >
+                        Test
+                    </div>
+                    <div
+                        className="home-page-card home-page-card-3"
+                        onClick={() => navigate("/forum")}
+                    >
+                        Forum
+                    </div>
+                </div>
             </section>
         </div>
     );
